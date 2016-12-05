fe511db95a56ddf59e0923352c91a47874deeffd
Michał Gurbiel <lachim@leibrug.pl>
2016-12-05 20:40:40 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// Theme initialization
+function theme_enqueue_styles() {
+	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+// JavaScript
+function theme_enqueue_scripts() {
+	if (is_page('cv')) {
+		wp_enqueue_script('mustache', get_stylesheet_directory_uri() . '/mustache.min.js');
+	}
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
+
+?>
