ba6f5717c1223b788751677c02aced760f7a8769
Michał Gurbiel <lachim@leibrug.pl>
2016-12-06 05:54:59 +0100

    Enqueue cover CSS

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@
 // Theme initialization
 function theme_enqueue_styles() {
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  if (is_home()) {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/cover/cover.css');
+  }
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
