b586c760bc9e5d9e25d7a1132cc54796804b3258
Michał Gurbiel <lachim@leibrug.pl>
2016-12-08 21:13:43 +0100

    Enqueue cover styles

--- functions.php
+++ functions.php
@@ -3,11 +3,14 @@
 // Theme initialization
 function theme_enqueue_styles() {
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+function cover_enqueue_styles() {
   if (is_home()) {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/cover/cover.css');
+    wp_enqueue_style('cover-style', get_stylesheet_directory_uri() . '/cover/cover.css');
   }
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+add_action('wp_enqueue_scripts', 'cover_enqueue_styles', 20);
 
 // JavaScript
 function theme_enqueue_scripts() {
